562942623bb634e5159384bb151e46ffda441438
Guy Farley <guy.e.farley@gmail.com>
2022-08-31 14:48:58 -0700

    Added test image to cards

--- components/Fly.js
+++ components/Fly.js
@@ -2,16 +2,21 @@
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import styles from '../components/layout.module.css';
+import Image from 'next/image';
 
 export default function Fly(item) {
   return (
-    <div className={styles.card}>
-      <Link href={`/items/[id]`} as={`/items/${item.id}`}>{item.title}</Link>
-      <br />
-      {item.id}
-      <br />
-      {item.price}
-      <br />
-    </div>
+    <section className={styles.card}>
+      <Image src="/copper_john.jpg" alt="test image" layout="responsive" width="100vw" height="70vw" />
+      <div>
+
+        <Link href={`/items/[id]`} as={`/items/${item.id}`}>{item.title}</Link>
+        <br />
+        {item.id}
+        <br />
+        {item.price}
+        <br />
+      </div>
+    </section>
   )
 }
